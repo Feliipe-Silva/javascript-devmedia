26b4bdfc29c967712b212674fd4584b38a47d2af
Felipe Silva <150200576+Feliipe-Silva@users.noreply.github.com>
2024-12-07 14:00:37 -0300

    Update nativas_func_arrays.js
    Função .forEach();

--- nativas_func_arrays.js
+++ nativas_func_arrays.js
@@ -15,6 +15,7 @@ array.splice(3, 0, "quanta-feira") // Adicionando elemento na posição 4 (indic
 
 array.splice(2, 0, "terça-feira") // Substituindo elemento
 console.log(array);
+console.log('\n');
 
 
 //          A função .splice()  ------CÓDIGO FORNECIDO COMO EXEMPLO---------
@@ -37,4 +38,43 @@ carrinho.splice(1,1);
 const totalElementos = carrinho.length;
 carrinho.splice(0,totalElementos);
 console.log(carrinho);      // ARRAY SEM VALORES
+console.log('\n');
+//------------------------------------------------------------------------------
+
+
+//          A função .forEach()  -------CÓDIGO FORNECIDO COMO EXEMPLO---------
+const produtos = [
+    { id: 1, nome: 'Açucar', estoque: 100, valor: 2.00 },
+    { id: 2, nome: 'Álcool 70', estoque: 50, valor: 9.95 },
+    { id: 3, nome: 'Luvas descartáveis', estoque: 1000, valor: 2.50 },
+];
+
+function imprimirProduto (produto, index) {
+    console.log( index + ' - ' + produto.nome );
+};
+
+produtos.forEach(imprimirProduto);
+console.log('\n');
+
+//-------------------------CONTINUAÇÃO---------------------------------
+
+
+const carrinho0 = [
+    {  nome: 'The Legend of Zelda', qnt: 1, valor: 250 },
+    {  nome: 'Super Mario Kart 8', qnt: 1, valor: 300 },
+    {  nome: 'New Super Mario Bros.', qnt: 1, valor: 250 }
+];
+
+function imprimirItem( produto, index ) {
+    let texto = index + ' - ';
+    texto += produto.qnt + ' UN - ';
+    texto += produto.nome + ' - ';
+    texto += 'R$ ' + produto.valor + ' - ';
+    texto += 'R$ ' + produto.qnt * produto.valor;
+
+    console.log( texto );
+};
+
+carrinho0.forEach(imprimirItem);
+console.log('\n');
 //------------------------------------------------------------------------------
