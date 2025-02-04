178ff5ce223353b561f7253d65d4abeb3f448b36
Felipe Silva <150200576+Feliipe-Silva@users.noreply.github.com>
2025-02-04 13:36:42 -0300

    Update colecao_de_frutas.js

--- atividades_propostas_js/jodo_da_forca/colecao_de_frutas.js
+++ atividades_propostas_js/jodo_da_forca/colecao_de_frutas.js
@@ -1,5 +1,4 @@
-//-------------------
-
+// --------------------------
 const listaDeFrutas = [
     'Pera', 'Banana', 'Laranja', 'Abacaxi',
     'Manga', 'Uva', 'Abacate', 'Jaca', 'Caju',
@@ -10,7 +9,7 @@ const listaDeFrutas = [
     'Groselha', 'Figo', 'Framboesa', 'Caqui', 'Cacau'
 ];
 
-function fruta_aleatoria() {
+function fruta_aleatoria( ) {
 
     let comprimento_array = listaDeFrutas.length;
     let num_aleatorio = Math.floor( Math.random(0, 1) * comprimento_array );
@@ -19,7 +18,7 @@ function fruta_aleatoria() {
 };
 
 let fruta_selecionada = fruta_aleatoria();
-let qtd_letras = fruta.length;
+let qtd_letras = fruta_selecionada.length;
 
 
 export { fruta_selecionada, qtd_letras };
